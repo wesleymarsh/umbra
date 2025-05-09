3271b36d816f43fbd4763166abf916a9db379734
Wesley Marsh <s242350@students.aie.edu.au>
2025-05-09 15:29:13 +1000

    Finished Screen class

--- umbra/Umbra/Screen.cpp
+++ umbra/Umbra/Screen.cpp
@@ -1,1 +1,88 @@
 #include "Screen.h"
+
+float Screen::Width() const
+{
+	return static_cast<float>(m_width);
+}
+
+float Screen::Height() const
+{
+	return static_cast<float>(m_height);
+}
+
+const string& Screen::Title() const
+{
+	return m_title;
+}
+
+const Color& Screen::ClearColor() const
+{
+	return m_clearColor;
+}
+
+void Screen::SetWidth(int newWidth)
+{
+	m_width = newWidth;
+	SetWindowSize(m_width, m_height);
+}
+
+void Screen::SetHeight(int newHeight)
+{
+	m_height = newHeight;
+	SetWindowSize(m_width, m_height);
+}
+
+void Screen::SetTitle(const char* newTitle)
+{
+	m_title = newTitle;
+	SetWindowTitle(m_title.c_str());
+}
+
+void Screen::SetClearColor(Color newClearColor)
+{
+	m_clearColor = newClearColor;
+}
+
+Screen::Screen()
+	: m_width { 800 }, m_height { 600 }, m_title { "umbra" }, m_clearColor { RAYWHITE },
+	m_isUsingAudio { false }
+{
+}
+
+Screen::~Screen() = default;
+
+// Returns if window opened successfully
+bool Screen::Open(bool initialiseAudio)
+{
+	m_isUsingAudio = initialiseAudio;
+
+	InitWindow(m_width, m_height, m_title.c_str());
+
+	if (m_isUsingAudio)
+	{
+		InitAudioDevice();
+	}
+
+	return IsWindowReady();
+}
+
+void Screen::Close()
+{
+	if (m_isUsingAudio)
+	{
+		CloseAudioDevice();
+	}
+
+	CloseWindow();
+}
+
+void Screen::NewFrame()
+{
+	BeginDrawing();
+	ClearBackground(m_clearColor);
+}
+
+void Screen::EndFrame()
+{
+	EndDrawing();
+}
--- umbra/Umbra/Screen.h
+++ umbra/Umbra/Screen.h
@@ -1,6 +1,45 @@
 #pragma once
 
+#include <raylib/raylib.h>
+
+#include <string>
+
+using std::string;
+
 class Screen
 {
+	friend class Application;
 
+public:
+	float Width() const;
+	float Height() const;
+
+	const string& Title() const;
+	const Color& ClearColor() const;
+
+	void SetWidth(int newWidth);
+	void SetHeight(int newHeight);
+
+	void SetTitle(const char* newTitle);
+	void SetClearColor(Color newClearColor);
+
+private:
+	int m_width;
+	int m_height;
+
+	string m_title;
+	Color m_clearColor;
+
+	bool m_isUsingAudio;
+
+private:
+	Screen();
+	~Screen();
+
+private:
+	bool Open(bool initialiseAudio = false);
+	void Close();
+
+	void NewFrame();
+	void EndFrame();
 };
