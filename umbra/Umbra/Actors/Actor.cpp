cacfd64abbd4b4d29c5d12bd53c3d4b720804d4c
Wesley Marsh <s242350@students.aie.edu.au>
2025-06-19 14:04:38 +1000

    Finished Actor class

--- umbra/Umbra/Actors/Actor.cpp
+++ umbra/Umbra/Actors/Actor.cpp
@@ -1,1 +1,40 @@
 #include "Actor.h"
+
+#include "ActorTransform.h"
+
+Actor::Actor()
+	: m_transform { new ActorTransform }
+{
+}
+
+Actor::Actor(Vector2 location, Vector2 scale, float rotation)
+	: m_transform{ new ActorTransform{ location, scale, rotation } }
+{
+}
+
+Actor::~Actor()
+{
+	delete m_transform;
+	m_transform = nullptr;
+}
+
+ActorTransform* Actor::Transform() const
+{
+	return m_transform;
+}
+
+void Actor::BeginPlay()
+{
+}
+
+void Actor::Tick(float dt)
+{
+}
+
+void Actor::Render()
+{
+}
+
+void Actor::EndPlay()
+{
+}
--- umbra/Umbra/Actors/Actor.h
+++ umbra/Umbra/Actors/Actor.h
@@ -1,6 +1,29 @@
 #pragma once
 
+#include <raylib/raylib.h>
+
+class ActorTransform;
+
 class Actor
 {
+	friend class ActorWorld;
 
+public:
+	Actor();
+	Actor(Vector2 location, Vector2 scale, float rotation = 0.f);
+	virtual ~Actor();
+
+public:
+	ActorTransform* Transform() const;
+
+protected:
+	virtual void BeginPlay();
+
+	virtual void Tick(float dt);
+	virtual void Render();
+
+	virtual void EndPlay();
+
+private:
+	ActorTransform* m_transform;
 };
