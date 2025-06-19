709f22148e6d84c65a453fbc29145d11138b056e
Wesley Marsh <s242350@students.aie.edu.au>
2025-06-19 13:59:47 +1000

    Finished ActorTransform class

--- umbra/Umbra/Actors/ActorTransform.cpp
+++ umbra/Umbra/Actors/ActorTransform.cpp
@@ -1,1 +1,40 @@
 #include "ActorTransform.h"
+
+#include <raylib/raymath.h>
+
+ActorTransform::ActorTransform()
+	: ActorTransform{ Vector2{ 0.f, 0.f } }
+{
+}
+
+ActorTransform::ActorTransform(Vector2 location)
+	: ActorTransform{ location, Vector2{ 0.f, 0.f } }
+{
+}
+
+ActorTransform::ActorTransform(Vector2 location, Vector2 scale)
+	: ActorTransform{ location, scale, 0.f }
+{
+}
+
+ActorTransform::ActorTransform(Vector2 location, Vector2 scale, float rotation)
+	: location{ location }, scale{ scale }, rotation{ rotation }
+{
+	Update();
+}
+
+Vector2 ActorTransform::Forward() const
+{
+	return Vector2Transform({ 0.f, -1.f }, m_transform);
+}
+
+Vector2 ActorTransform::Right() const
+{
+	return Vector2Transform({ 1.f, 0.f }, m_transform);
+}
+
+void ActorTransform::Update()
+{
+	m_transform = MatrixTranslate(location.x, location.y, 0.f) *
+		MatrixRotateZ(rotation * DEG2RAD) * MatrixScale(scale.x, scale.y, 1.f);
+}
--- umbra/Umbra/Actors/ActorTransform.h
+++ umbra/Umbra/Actors/ActorTransform.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include <raylib/raylib.h>
+
 class ActorTransform
 {
+public:
+	Vector2 location;
+	Vector2 scale;
+	float rotation;
 
+public:
+	ActorTransform();
+	ActorTransform(Vector2 location);
+	ActorTransform(Vector2 location, Vector2 scale);
+	ActorTransform(Vector2 location, Vector2 scale, float rotation);
+
+public:
+	Vector2 Forward() const;
+	Vector2 Right() const;
+
+private:
+	Matrix m_transform;
+
+private:
+	void Update();
 };
