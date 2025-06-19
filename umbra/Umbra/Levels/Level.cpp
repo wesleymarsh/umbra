1d93e0a4d3d6299e3d58a42ec9626c716fd4ebbd
Wesley Marsh <s242350@students.aie.edu.au>
2025-06-19 14:29:59 +1000

    Finished Level class

--- umbra/Umbra/Levels/Level.cpp
+++ umbra/Umbra/Levels/Level.cpp
@@ -1,1 +1,40 @@
 #include "Level.h"
+
+#include "Umbra/Actors/ActorWorld.h"
+
+string Level::Name() const
+{
+	return m_name;
+}
+
+ActorWorld const* Level::World() const
+{
+	return m_world;
+}
+
+Level::Level(string name)
+	: m_world { new ActorWorld }, m_name { std::move(name) }
+{
+}
+
+Level::~Level()
+{
+	delete m_world;
+	m_world = nullptr;
+}
+
+void Level::OnLoaded()
+{
+}
+
+void Level::OnUnloaded()
+{
+}
+
+void Level::Tick(float dt)
+{
+}
+
+void Level::Render()
+{
+}
--- umbra/Umbra/Levels/Level.h
+++ umbra/Umbra/Levels/Level.h
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <string>
+
+using std::string;
+
+class ActorWorld;
+
 class Level
 {
+	friend class LevelManager;
 
+public:
+	string Name() const;
+	ActorWorld const* World() const;
+
+protected:
+	explicit Level(string name);
+	virtual ~Level();
+
+private:
+	virtual void OnLoaded();
+	virtual void OnUnloaded();
+
+	virtual void Tick(float dt);
+	virtual void Render();
+
+private:
+	ActorWorld* m_world;
+	string m_name;
 };
